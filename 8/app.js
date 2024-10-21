78dc773dd49c44d58e6b602637b37f422b48cade
NEO <neo.uz001@gmail.com>
2024-10-21 17:58:09 +0500

    Finished Clearly

--- 8/app.js
+++ 8/app.js
@@ -1,9 +1,3 @@
-let user1 = prompt("So'z kiriting");
-
-let user2 = user1.split('').reverse().join('');
-
-if (user1 === user2) {
-    alert("true");
-} else {
-    alert("false");
-}
+let word = prompt("So'z kiriting");
+let reversed = word.split('').reverse().join('');
+alert(word === reversed);
